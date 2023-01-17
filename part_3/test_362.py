38852fdc9f20f354723d3f9adef4d350f259ffb9
yurii.kotliar <yukotliar@gmail.com>
2023-01-17 12:45:03 +0200

    small changes and 362 added

--- part_3/test_362.py
+++ part_3/test_362.py
@@ -1,10 +1,23 @@
+import pytest
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+import time
+import math
 
-link = "https://stepik.org/lesson/236895/step/1"
+answer = int(math.log(int(time.time())))
 
-def test_login(browser):
+links = ["https://stepik.org/lesson/236895/step/1",
+        "https://stepik.org/lesson/236896/step/1",
+        "https://stepik.org/lesson/236897/step/1",
+        "https://stepik.org/lesson/236898/step/1",
+        "https://stepik.org/lesson/236899/step/1",
+        "https://stepik.org/lesson/236903/step/1",
+        "https://stepik.org/lesson/236904/step/1",
+        "https://stepik.org/lesson/236905/step/1"]
+
+@pytest.mark.parametrize('link', links)
+def test_parametrize(browser, link):
     browser.get(link)
     button = WebDriverWait(browser, 5).until(
             EC.element_to_be_clickable((By.ID, "ember33"))
@@ -19,3 +32,8 @@ def test_login(browser):
     pop_up = WebDriverWait(browser,5).until(
         EC.invisibility_of_element_located((By.ID, "ember84"))
     )
+    text_field = browser.find_element(By.ID, "ember99")
+    text_field.send_keys(answer)
+
+
+
