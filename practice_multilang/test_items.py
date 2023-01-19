e4f3c23c44555b4737f7cc7924b5361779368a34
yurii.kotliar <yukotliar@gmail.com>
2023-01-19 23:15:35 +0200

    test items added

--- practice_multilang/test_items.py
+++ practice_multilang/test_items.py
@@ -7,5 +7,6 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_basket_button_present(browser):
     browser.get(link)
-    basket_button = browser.find_element(By.XPATH, "//button[contains(@class, 'btn-add-to-basked')]")
+    basket_button = browser.find_elements(By.XPATH, "//button[contains(@class, 'btn-add-to-baskett')]")
+    assert len(basket_button) > 0, "Basket button isn't found"
     time.sleep(5)
