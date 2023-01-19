41d61fbc0c068a1663272d988ce1e75c1ba07303
yurii.kotliar <yukotliar@gmail.com>
2023-01-19 20:51:42 +0200

    test items added

--- practice_multilang/test_items.py
+++ practice_multilang/test_items.py
@@ -1,8 +1,11 @@
+import time
+
 from selenium.webdriver.common.by import By
 
-link = "http://selenium1py.pythonanywhere.com/"
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_guest_should_see_login_link(browser):
+def test_basket_button_present(browser):
     browser.get(link)
-    browser.find_element(By.CSS_SELECTOR, "#login_link")
+    basket_button = browser.find_element(By.XPATH, "//button[contains(@class, 'btn-add-to-basked')]")
+    time.sleep(5)
